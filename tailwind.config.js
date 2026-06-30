e1a29a2ca51db5ee344fdc3790111b1a3ffd59d2
eusoutta <eusoutta@gmail.com>
2026-06-30 22:44:36 +0200

    Fix tailwind config ESM import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -152,5 +154,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
